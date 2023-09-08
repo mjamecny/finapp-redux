534ed83721cd373e3b6a9570a96c80bb0685ec94
mjamecny <jamecny.m@tutanota.com>
2023-09-08 18:43:20 +0200

    add div container and styles for welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,3 +1,13 @@
+import styled from "styled-components"
+
+const StyledWelcome = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-block: 1.8rem;
+  height: 100vh;
+  gap: 2rem;
+`
+
 export default function Welcome() {
-  return <div>Welcome</div>
+  return <StyledWelcome>Welcome</StyledWelcome>
 }
