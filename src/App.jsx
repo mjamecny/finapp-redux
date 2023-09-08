a4974082348f6073f893d52ea3a88b0786b2e0b9
mjamecny <jamecny.m@tutanota.com>
2023-09-08 18:32:33 +0200

    add browser router and welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Welcome from "./pages/Welcome"
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Welcome />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
