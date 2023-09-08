3fb1b891e8d81c690d8064820601aef84fc162c2
mjamecny <jamecny.m@tutanota.com>
2023-09-08 19:21:36 +0200

    add text box and styles for it in welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 import Logo from "../ui/Logo"
+import Heading from "../ui/Heading"
 
 const StyledWelcome = styled.div`
   display: flex;
@@ -19,6 +20,18 @@ const WelcomeImage = styled.img`
   width: 60%;
 `
 
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-inline: 2rem;
+  gap: 1.2rem;
+`
+
+const Info = styled.p`
+  font-size: 1.8rem;
+  color: #868e96;
+`
+
 export default function Welcome() {
   return (
     <StyledWelcome>
@@ -26,6 +39,12 @@ export default function Welcome() {
       <ImageBox>
         <WelcomeImage src="./welcome_image.svg" alt="Person with graphs" />
       </ImageBox>
+      <TextBox>
+        <Heading as="h1">Easy way to manage your transactions</Heading>
+        <Info>
+          Track your financial transactions for three different accounts.
+        </Info>
+      </TextBox>
     </StyledWelcome>
   )
 }
