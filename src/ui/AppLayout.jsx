4d94f05cd6a6467485b209521211958d948d5abf
mjamecny <jamecny.m@tutanota.com>
2023-09-08 21:56:26 +0200

    create header component, styles for it and import to applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components"
 import { Outlet } from "react-router-dom"
+
 import Footer from "./Footer"
+import Header from "./Header"
 
 const StyledAppLayout = styled.div`
   display: grid;
@@ -12,6 +14,7 @@ const StyledAppLayout = styled.div`
 export default function AppLayout() {
   return (
     <StyledAppLayout>
+      <Header />
       <Outlet />
       <Footer />
     </StyledAppLayout>
---
+++ src/ui/Header.jsx
@@ -0,0 +1,37 @@
+import { styled } from "styled-components"
+import { FaBars, FaX } from "react-icons/fa6"
+import { useState } from "react"
+
+import Logo from "./Logo"
+
+const StyledHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 1.2rem;
+  padding: 1.2rem 0rem;
+  font-size: 1.6rem;
+  position: relative;
+
+  & svg {
+    cursor: pointer;
+    width: 2.2rem;
+    height: 2.2rem;
+  }
+`
+
+export default function Header() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  return (
+    <StyledHeader>
+      <Logo type="medium" />
+
+      {showMenu ? (
+        <FaX onClick={() => setShowMenu((showMenu) => !showMenu)} />
+      ) : (
+        <FaBars onClick={() => setShowMenu((showMenu) => !showMenu)} />
+      )}
+    </StyledHeader>
+  )
+}
