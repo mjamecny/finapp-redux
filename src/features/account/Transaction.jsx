873363e56196fb2bed01281960a1ca28ae819f96
mjamecny <jamecny.m@tutanota.com>
2023-09-09 21:45:40 +0200

    add Transaction component

---
+++ src/features/account/Transaction.jsx
@@ -0,0 +1,116 @@
+import styled, { css } from "styled-components"
+import { Link, useNavigate } from "react-router-dom"
+import { useSelector } from "react-redux"
+
+import { convertToDDMMYYYY, getCurrency } from "../../utils/helpers"
+
+import AccountIcon from "../../ui/AccountIcon"
+import ActionButton from "../../ui/ActionButton"
+
+const StyledTransaction = styled.div`
+  color: var(--color-grey-font-900);
+  border: 1px solid var(--color-grey-font-900);
+  font-size: 1.4rem;
+  padding: 0.8rem 0.6rem;
+  border-radius: 7px;
+`
+
+const Price = styled.p`
+  font-size: 1.2rem;
+  font-weight: 600;
+
+  ${(props) =>
+    props.type === "withdraw" &&
+    css`
+      color: red;
+    `}
+  ${(props) =>
+    props.type === "deposit" &&
+    css`
+      color: green;
+    `}
+`
+
+const TransactionRowHorizontal = styled.div`
+  display: flex;
+  gap: 1rem;
+  align-items: center;
+`
+
+const TransactionRowVertical = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 1.2rem;
+  gap: 0.4rem;
+`
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-weight: 600;
+  color: var(--color-grey-font-900);
+`
+
+const ActionButtonContainer = styled.div`
+  display: flex;
+  gap: 0.4rem;
+`
+
+const CreatedAt = styled.p`
+  font-size: 1rem;
+  color: #868e96;
+`
+
+export default function Transaction({ transaction }) {
+  const currency = useSelector((state) => state.user.currency)
+  const currencyLabel = getCurrency(currency)
+  const { accountType, description, amount, id, created_at } = transaction
+  const navigate = useNavigate()
+
+  return (
+    <StyledTransaction>
+      <TransactionRowHorizontal>
+        <div style={{ display: "flex", gap: "0.8rem", alignItems: "center" }}>
+          <AccountIcon type={accountType} size="small" colored={true} />
+          <TransactionRowVertical>
+            <StyledLink to={`/transaction/${id}`}>{description}</StyledLink>
+            <CreatedAt>
+              {created_at === new Date()
+                ? "Today"
+                : convertToDDMMYYYY(created_at)}
+            </CreatedAt>
+          </TransactionRowVertical>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            gap: "0.8rem",
+            marginLeft: "auto",
+            alignItems: "center",
+          }}
+        >
+          {amount < 0 ? (
+            <Price type="withdraw">
+              {accountType === "Bitcoin"
+                ? amount
+                : `${amount} ${currencyLabel}`}
+            </Price>
+          ) : (
+            <Price type="deposit">
+              {accountType === "Bitcoin"
+                ? amount
+                : `${amount} ${currencyLabel}`}
+            </Price>
+          )}
+
+          <ActionButtonContainer>
+            <>
+              <ActionButton type="edit" size="small" />
+              <ActionButton type="delete" size="small" />
+            </>
+          </ActionButtonContainer>
+        </div>
+      </TransactionRowHorizontal>
+    </StyledTransaction>
+  )
+}
