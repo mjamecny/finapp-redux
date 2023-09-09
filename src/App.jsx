add24cf9832c92c474556d15834de0dd92b30e60
mjamecny <jamecny.m@tutanota.com>
2023-09-09 12:23:01 +0200

    create addaccount component and import to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Welcome from "./pages/Welcome"
 import GlobalStyles from "./styles/GlobalStyles"
 import Dashboard from "./pages/Dashboard"
 import AppLayout from "./ui/AppLayout"
+import AddAccount from "./features/account/AddAccount"
 
 import { DarkModeProvider } from "./context/DarkModeContext"
 
@@ -16,6 +17,7 @@ export default function App() {
           <Routes>
             <Route element={<AppLayout />}>
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="account/add" element={<AddAccount />} />
             </Route>
             <Route index element={<Welcome />} />
           </Routes>
---
+++ src/features/account/AddAccount.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+const StyledAddAccount = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2.4rem;
+`
+
+export default function AddAccount() {
+  return <StyledAddAccount>add account</StyledAddAccount>
+}
