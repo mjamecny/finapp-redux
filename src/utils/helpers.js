97c236208351191954357419f10509c8359b6471
mjamecny <jamecny.m@tutanota.com>
2023-09-09 21:07:20 +0200

    add convertToDDMMYYYY function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -10,3 +10,13 @@ export function getCurrency(currency) {
       return "USD"
   }
 }
+
+export function convertToDDMMYYYY(timestamp) {
+  const dateObj = new Date(timestamp)
+
+  const day = String(dateObj.getDate()).padStart(2, "0")
+  const month = String(dateObj.getMonth() + 1).padStart(2, "0")
+  const year = dateObj.getFullYear()
+
+  return `${day}/${month}/${year}`
+}
