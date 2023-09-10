5274a3ff84e2fd0a36ec713ba1bfa773fc2b823a
mjamecny <jamecny.m@tutanota.com>
2023-09-10 16:32:20 +0200

    add Dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,32 @@
+import { useSelector } from "react-redux"
+import { useTranslation } from "react-i18next"
+
+import Empty from "../ui/Empty"
+import Row from "../ui/Row"
+import AccountList from "../features/account/AccountList"
+import TransactionList from "../features/account/TransactionsList"
+
+import { getTransactions } from "../features/account/accountSlice"
+
 export default function Dashboard() {
-  return <div>Dashboard</div>
+  const accounts = useSelector((state) => state.account.accounts)
+  const transactions = useSelector(getTransactions)
+  const { t } = useTranslation()
+
+  return (
+    <>
+      {accounts.length === 0 ? (
+        <Empty
+          message={t("empty.accounts")}
+          buttonLabel={t("empty.add_account")}
+          path="/account/add"
+        />
+      ) : (
+        <Row type="vertical">
+          <AccountList accounts={accounts} />
+          <TransactionList transactions={transactions} />
+        </Row>
+      )}
+    </>
+  )
 }
