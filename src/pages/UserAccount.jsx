641709641c322b11e434a0fa941720fae864774a
mjamecny <jamecny.m@tutanota.com>
2023-09-11 17:21:07 +0200

    add styles for UserAccount

--- src/pages/UserAccount.jsx
+++ src/pages/UserAccount.jsx
@@ -1,9 +1,18 @@
 import { useSelector } from "react-redux"
+import styled from "styled-components"
 
 import Button from "../ui/Button"
 import Heading from "../ui/Heading"
 import Form from "../ui/Form"
 
+const StyledExport = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 2.4rem;
+  background-color: var(--color-grey-back-900);
+  height: 100vh;
+`
 export default function UserAccount() {
   const user = useSelector((state) => state.user)
   const accounts = useSelector((state) => state.account.accounts)
@@ -23,11 +32,11 @@ export default function UserAccount() {
   }
 
   return (
-    <div>
+    <StyledExport>
       <Heading as="h2">Export data</Heading>
       <Form onSubmit={handleExport}>
         <Button size="small">Export</Button>
       </Form>
-    </div>
+    </StyledExport>
   )
 }
