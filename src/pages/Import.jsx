28298c353ead9facdf915041890bbdd34888d805
mjamecny <jamecny.m@tutanota.com>
2023-09-11 16:06:49 +0200

    add translation for Import page

--- src/pages/Import.jsx
+++ src/pages/Import.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import toast from "react-hot-toast"
+import { useTranslation } from "react-i18next"
 import styled from "styled-components"
 
 import { setAccounts } from "../features/account/accountSlice"
@@ -27,6 +28,7 @@ export default function Import() {
   const [data, setData] = useState(null)
   const dispatch = useDispatch()
   const navigate = useNavigate()
+  const { t } = useTranslation()
 
   const handleImport = (e) => {
     e.preventDefault()
@@ -41,9 +43,9 @@ export default function Import() {
         dispatch(setUserCurrency(data.currency))
         dispatch(setAccounts(data.accounts))
         navigate("/dashboard")
-        toast.success("Data successfully imported")
+        toast.success(t("import.toast"))
       } catch (error) {
-        toast.error("Invalid file format")
+        toast.error(t("import.toast_error"))
         setData(null)
       }
     }
@@ -52,16 +54,16 @@ export default function Import() {
 
   return (
     <StyledImport>
-      <Heading as="h2">Import user data</Heading>
+      <Heading as="h2">{t("import.heading")}</Heading>
       <Form onSubmit={handleImport}>
-        <FormRow label="User data" id="userData">
+        <FormRow label={t("import.user_data_label")} id="userData">
           <FileInput
             id="userData"
             accept="json"
             onChange={(e) => setData(e.target.files[0])}
           />
         </FormRow>
-        <Button size="small">Import</Button>
+        <Button size="small">{t("import.button_label")}</Button>
       </Form>
       <ButtonHome />
     </StyledImport>
